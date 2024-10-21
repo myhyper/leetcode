47383af592688329f886c4dd9d91790c38126ba9
myhyper <myeongje@gmail.com>
2024-10-21 23:52:25 +0900

    simplify_path solved

--- lib/my_leet.rb
+++ lib/my_leet.rb
@@ -1,23 +1,33 @@
 # frozen_string_literal: true
 $dbg = `uname -a`.include?('Darwin') # set debug mode only for mac
 
-
-# @param {Integer[]} chalk
-# @param {Integer} k
-# @return {Integer}
-def chalk_replacer(chalk, k)
-  total_chalk = chalk.sum
-
-  k %= total_chalk
-
-  chalk.each_with_index do |c, idx|
-    return idx if k < c
-    k -= c
+# @param {String} path
+# @return {String}
+def simplify_path(path)
+  # Split the path into components by '/' and process each component
+  components = path.split('/')
+  
+  # Use a stack to handle directory navigation
+  stack = []
+  
+  components.each do |component|
+    case component
+    when '', '.'  # Ignore empty strings and '.' (current directory)
+      next
+    when '..'  # '..' means go up to the parent directory
+      stack.pop unless stack.empty?
+    else  # Valid directory or file name
+      stack.push(component)
+    end
   end
+  
+  # Join the stack to form the canonical path
+  # If stack is empty, return '/'
+  '/' + stack.join('/')
 end
 
 class MyLeet
-  def self.run chalk, k
-    chalk_replacer chalk, k
+  def self.run path
+    simplify_path path
   end
 end
--- spec/my_leet_spec.rb
+++ spec/my_leet_spec.rb
@@ -6,23 +6,20 @@ describe MyLeet do
   context '.run' do
     it 'test case 0' do
       expect(MyLeet.run(
-        [5,1,5],
-        22
-      )).to eq 0
+        "/../"
+      )).to eq '/'
     end
 
     it 'test case 1' do
       expect(MyLeet.run(
-        [3,4,1,2],
-        25
-      )).to eq 1
+        "/.../a/../b/c/../d/./"
+      )).to eq "/.../b/d"
     end
 
     it 'test case 2' do
       expect(MyLeet.run(
-        [16,29,70,14,81,16,21,70,66,45,65,37,72,58,58,98,63,6,61,1,18,37,97,90,100,75,59,73,50,100],
-        940635372
-      )).to eq 14
+        "/home/user/Documents/../Pictures"
+      )).to eq "/home/user/Pictures"
     end
     
   end
