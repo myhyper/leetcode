a75be03246a9e690189f7f4755c8436cb868fd67
myhyper <myeongje@gmail.com>
2024-10-21 23:55:09 +0900

    simplify_path solved

--- lib/my_leet.rb
+++ lib/my_leet.rb
@@ -4,25 +4,21 @@ $dbg = `uname -a`.include?('Darwin') # set debug mode only for mac
 # @param {String} path
 # @return {String}
 def simplify_path(path)
-  # Split the path into components by '/' and process each component
+
   components = path.split('/')
-  
-  # Use a stack to handle directory navigation
   stack = []
   
   components.each do |component|
     case component
-    when '', '.'  # Ignore empty strings and '.' (current directory)
+    when '', '.'
       next
-    when '..'  # '..' means go up to the parent directory
+    when '..'
       stack.pop unless stack.empty?
-    else  # Valid directory or file name
+    else
       stack.push(component)
     end
   end
   
-  # Join the stack to form the canonical path
-  # If stack is empty, return '/'
   '/' + stack.join('/')
 end
 
